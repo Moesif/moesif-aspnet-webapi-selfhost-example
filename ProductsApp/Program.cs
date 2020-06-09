6a2c0c82a571a03512e98ad48d1d2c7525d8663c
Derric Gilling <derric@moesif.com>
2020-06-09 05:01:17 +0000

    Initial commit

---
+++ ProductsApp/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+using Owin;
+using System.Web.Http;
+using Topshelf;
+using Moesif.Middleware;
+using ProductsApp.Settings;
+
+namespace ProductsApp
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            return (int)HostFactory.Run(x =>
+            {
+                x.Service<OwinService>(s =>
+                {
+                    s.ConstructUsing(() => new OwinService());
+                    s.WhenStarted(service => service.Start());
+                    s.WhenStopped(service => service.Stop());
+                });
+            });
+        }
+    }
+
+    public class OwinService
+    {
+        private IDisposable _webApp;
+
+        public void Start()
+        {
+            _webApp = WebApp.Start<StartOwin>("http://localhost:9000");
+        }
+
+        public void Stop()
+        {
+            _webApp.Dispose();
+        }
+    }
+
+    public class StartOwin
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            appBuilder.Use<MoesifMiddleware>(MoesifOptions.moesifOptions);
+
+            var config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+                );
+
+            appBuilder.UseWebApi(config);
+        }
+    }
+
+    public class HelloWorldController : ApiController
+    {
+        public string Get()
+        {
+            return "Hello, World!";
+        }
+    }
+}
---
+++ ProductsApp/Settings/MoesifOptions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+using Microsoft.Owin;
+using Moesif.Api.Models;
+
+namespace ProductsApp.Settings
+{
+    public class MoesifOptions
+    {
+        public static Func<IOwinRequest, IOwinResponse, string> IdentifyUser = (IOwinRequest req, IOwinResponse res) => {
+            // Implement your custom logic to return user id
+            return req?.Context?.Authentication?.User?.Identity?.Name;
+        };
+
+        public static Func<IOwinRequest, IOwinResponse, string> IdentifyCompany = (IOwinRequest req, IOwinResponse res) => {
+            return req.Headers["X-Organization-Id"];
+        };
+
+        public static Func<IOwinRequest, IOwinResponse, string> GetSessionToken = (IOwinRequest req, IOwinResponse res) => {
+            return req.Headers["Authorization"];
+        };
+
+        public static Func<IOwinRequest, IOwinResponse, Dictionary<string, object>> GetMetadata = (IOwinRequest req, IOwinResponse res) => {
+            Dictionary<string, object> metadata = new Dictionary<string, object>
+            {
+                {"string_field", "value_1"},
+                {"number_field", 0},
+                {"object_field", new Dictionary<string, string> {
+                    {"field_a", "value_a"},
+                    {"field_b", "value_b"}
+                    }
+                }
+            };
+            return metadata;
+        };
+
+        public static Func<HttpRequestMessage, HttpResponseMessage, Dictionary<string, object>> GetMetadataOutgoing = (HttpRequestMessage req, HttpResponseMessage res) => {
+            Dictionary<string, object> metadata = new Dictionary<string, object>
+            {
+                {"string_field", "value_1"},
+                {"number_field", 0},
+                {"object_field", new Dictionary<string, string> {
+                    {"field_a", "value_a"},
+                    {"field_b", "value_b"}
+                    }
+                }
+            };
+            return metadata;
+        };
+
+        static public Dictionary<string, object> moesifOptions = new Dictionary<string, object>
+        {
+            {"ApplicationId", "Your Moesif Application Id"},
+            {"LocalDebug", true},
+            {"LogBody", true},
+            {"LogBodyOutgoing", true},
+            {"ApiVersion", "1.1.0"},
+            {"IdentifyUser", IdentifyUser},
+            {"IdentifyCompany", IdentifyCompany},
+            {"GetSessionToken", GetSessionToken},
+            {"GetMetadata", GetMetadata},
+            {"GetMetadataOutgoing", GetMetadataOutgoing},
+            {"EnableBatching", false},
+            {"BatchSize", 25}
+        };
+    }
+}
